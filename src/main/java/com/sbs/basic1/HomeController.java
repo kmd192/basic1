fc8adeeeacec1bb67e840217c2b9929aa362f249
kmd192 <kmd192@naver.com>
2024-04-25 16:30:12 +0900

    C:/Program Files/Git/usr/home/main2, /usr/home/main3 구현

--- src/main/java/com/sbs/basic1/HomeController.java
+++ src/main/java/com/sbs/basic1/HomeController.java
@@ -6,9 +6,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/home/main")
     @ResponseBody
     public String showHome(){
         return "안녕하세요.";
     }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showHome2(){
+        return "환영합니다.";
+    }
+
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showHome3(){
+        return "스프링부트는 획기적이다.";
+    }
+
 }
