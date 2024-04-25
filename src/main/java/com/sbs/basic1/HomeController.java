7cb08181dabffd14f1f204e9a6bf96e6e5f5c705
kmd192 <kmd192@naver.com>
2024-04-25 16:57:55 +0900

    C:/Program Files/Git/home/plus

--- src/main/java/com/sbs/basic1/HomeController.java
+++ src/main/java/com/sbs/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.sbs.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -36,6 +37,12 @@ public class HomeController {
     public int showIncrease(){
         count++;
         return count;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b){
+        return a + b;
     }
 
 }
